0e64114452ffa5e9f074e4ed751f16db28c495cf
Biome <WorkBiome@proton.me>
2023-10-20 00:18:11 +0200

    modif

--- Views/panier.php
+++ Views/panier.php
@@ -13,6 +13,7 @@
 </head>
 
 <body>
+
   <section class="h-100 h-custom" style="background-color: #eee;">
     <div class="container py-5 h-100">
       <div class="row d-flex justify-content-center align-items-center h-100">
@@ -274,6 +275,9 @@
       </div>
     </div>
   </section>
+  <!-- Footer-->
+  <?php require("footer.php"); ?>
+
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 
--- Views/produit.php
+++ Views/produit.php
@@ -175,6 +175,8 @@ $userConnected = false;
   </section>
 
   <?php require('footer.php'); ?>
+
+  
   <!-- Bootstrap core JS-->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
   <!-- Core theme JS-->
